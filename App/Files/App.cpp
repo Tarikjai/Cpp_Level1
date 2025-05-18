9db068997717fb284b94877836b01da8a6f02d17
Tarik <tarik.jaidani@gmail.com>
2025-05-18 13:17:51 +0200

    Function part1

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -7,29 +7,8 @@ using namespace std;
 
 
 
-
-
 int main()
 {
-	string String1;
-	string String2;
-	string String3;
 
-	cout << "Please Enter String1: " << endl;
-	getline(cin, String1);
-
-	cout << "Please Enter String2: " << endl;
-	cin >> String2  ;
-
-	cout << "Please Enter String2: " << endl;
-	cin >> String3;
-
-	cout << "***************************"<< endl;
-	cout << "The Length of String1 is " << String1.length() <<  endl;
-	cout << "Characters at 0,2,4,7 are:  " << String1[0] << String1[2] << String1[4] << String1[7] << endl;
-	cout << "Concatenatin String2 and String3= " << String2 + String3 << endl ;
-	cout << String2 << " * " << String3 << " = " << stoi(String2) * stoi(String3);
-
-   
 
 }
---
+++ App/Files/Functions_Part1.cpp
@@ -0,0 +1,52 @@
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+void displayMyCardInfo()
+{
+	cout << "**********************************" << endl;
+	cout << "Name: Mohammed Abu-Hadhoud.\n";
+	cout << "Age: 44 Years.\n";
+	cout << "City: Ammman.\n";
+	cout << "Country: Jordan.\n";
+	cout << "**********************************" << endl;
+}
+
+void PrintSquarStar()
+{
+	cout << "**********************************" << endl;
+	cout << "**********************************\n";
+	cout << "**********************************\n";
+	cout << "**********************************\n";
+	cout << "**********************************" << endl;
+}
+
+void ILoveProgramming()
+{
+	cout << "I Love Programming!\n";
+}
+
+void PrintH()
+{
+	cout << "*      *\n";
+	cout << "*      *\n";
+	cout << "********\n";
+	cout << "*      *\n";
+	cout << "*      *\n";
+}
+
+int main()
+{
+	displayMyCardInfo();
+	cout << "" << endl;
+	PrintSquarStar();
+	cout << "" << endl;
+	ILoveProgramming();
+	cout << "" << endl;
+	PrintH();
+
+}
