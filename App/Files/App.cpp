0b4bfd6d65c095bd2378441769be1d6d50131609
Tarik <tarik.jaidani@gmail.com>
2025-06-15 23:04:28 +0200

    33

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -2,12 +2,39 @@
 #include <string>
 using namespace std;
 
- 
 
- 
+int readGrade()
+{
+	int Grade;
+	cout << "Enter your Grade: " << endl;
+	cin >> Grade;
+	return Grade;
+}
+
+void printResult(int grade)
+{
+	if (grade >= 90 && grade <= 100)
+		cout << "A";
+	else if (grade >= 80 && grade <= 89)
+		cout << "B";
+	else if (grade >= 70 && grade <= 79)
+		cout << "C";
+	else if (grade >= 60 && grade <= 69)
+		cout << "D";
+	else if (grade >= 50 && grade <= 59)
+		cout << "E";
+	else  
+		cout << "F";
+}
+
+
 
 int main()
 {
-	 
-}
+	int grade = readGrade();
 
+	printResult(grade);
+
+
+
+}
