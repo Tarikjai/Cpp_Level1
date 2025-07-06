3389511f3608c4312c4f259abaeb45e44d9a7b8c
Tarik <tarik.jaidani@gmail.com>
2025-07-06 23:30:26 +0200

    44_1

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -2,7 +2,37 @@
 #include <string>
 using namespace std;
 
-int main()
+;
+enum enColor { Red=1, Blue=2, Green=3, Yellow=4};
+
+int  readAnswer(int &c)
 {
+	cout << "***********************" << endl;
+	cout << "PLease Choose the number of your color " << endl;
+	cout << "(1) Red" << endl;
+	cout << "(2) Blue" << endl;
+	cout << "(3) Green" << endl;
+	cout << "(4) Yellow" << endl;
+	cout << "***********************" << endl;
+
+	cout << "Your choice ? " << endl;
+	cin >> c;
+
+	return c;
 }
 
+void printResult(int c)
+{
+	cout << c << endl;
+}
+
+
+
+int main()
+{
+	int c;
+	readAnswer(c);
+	printResult(c);
+
+
+}
