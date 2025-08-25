dc785fb948c94de6b0534677e6f3a63649ada532
Tarik <tarik.jaidani@gmail.com>
2025-08-25 23:21:48 +0200

    48

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -2,32 +2,31 @@
 #include <string>
 using namespace std;
 
- 
-void readArray(int array[10],int& length)
+void readArray(int array[10], int& length)
 {
-	cout << "Saisir le nombre de numero de numeros: " << endl;
+	cout << "Please insert the number desired: " << endl;
 	cin >> length;
 
-	for (int i = 0; i <= length -1; i++)
-	{
-		cout << "Merci de saisir le numero: " << i + 1 << endl;
-		cin >> array[i];
-	}
-}
-
-void printArray(int array[10], int length)
-{
-	cout << "Voici la liste des numero composant notre Array: " << endl;
 	for (int i = 0; i <= length - 1; i++)
 	{
-		cout << array[i];
+		cout << "Please insert the [" << i + 1 << "] number: " << endl;
+		cin >> array[i];
+	}
+
+}
+
+void PrintArray(int array[10], int length)
+{
+	for (int i = 0; i <= length - 1; i++)
+	{
+		cout << "Number [" << i + 1 << "] is: " << array[i] << endl;
 	}
 }
 
 int main()
 {
-	int array[10], length = 0;
+	int array[10], length = 0 ;
+
 	readArray(array, length);
-	printArray(array, length);
-	
+	PrintArray(array, length);
 }
