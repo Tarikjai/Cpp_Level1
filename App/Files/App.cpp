fd24aada72283e461b16b146168c7539b51dd058
Tarik <tarik.jaidani@gmail.com>
2025-09-01 21:22:06 +0200

    49

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -2,81 +2,8 @@
 #include <string>
 using namespace std;
 
-//void printOne()
-//{
-//	for (char i = 65; i <= 90; i++)
-//	{
-//		for (char j = 65; j <= 90; j++)
-//		{
-//			cout << i << j << endl;
-//		}
-//	}
-//}
 
-//void printOne()
-//{
-//	for (int i = 1; i <= 10; i++)
-//	{
-//		for (int j = 1 ; j <= 10 -i ; j++)
-//		{
-//			cout << "*";
-//			
-//		}
-//		cout << endl;
-//	}
-//}
-
-//void print()
-//{
-//	for (int i = 0 ; i <= 10; i++)
-//	{
-//		for (int j = 1; j <= 10-i; j++)
-//		{
-//			cout << j << " ";
-//		}
-//		cout  << endl;
-//	}
-//}
-
-//void print()
-//{
-//	for (int i = 0; i <= 10; i++)
-//	{
-//		for (int j = 1; j <= i; j++)
-//		{
-//			cout << j << " ";
-//			
-//		}
-//		
-//		cout << endl;
-//	}
-//}
-
-//void print()
-//{
-//	for (char i = 65; i <= 70; i++)
-//	{
-//		for (char j = 65; j <= i; j++)
-//		{
-//			cout << j << " ";
-//		}
-//		cout << endl;
-//	}
-//}
-
-void print()
-{
-	for (int i = 1; i <= 10; i++)
-	{
-		for (int j = i; j <=10 ; j++)
-		{
-			cout << j<< " "; ;
-		}
-		cout  << endl;
-	}
-}
 int main()
 {
-	print();
-	cout << endl;
+	
 }
