5ab4c103faf3cb4c20569ff633d9869aec350dd7
Tarik <tarik.jaidani@gmail.com>
2025-09-06 13:01:29 +0200

    37

---
+++ App/Files/Problem_37.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void input(int& N)
+{
+
+	cout << "Please insert N: ";
+	cin >> N;
+
+
+}
+
+int Factorial(int N)
+{
+	int sum = 0;
+
+
+	while (N != -99)
+	{
+		sum += N;
+		input(N);
+	}
+	return sum;
+}
+
+int main()
+{
+	int N = 0;
+	input(N);
+	cout << Factorial(N);
+}
