f9b910eecd808e734528f097507428180e001fd9
Tarik <tarik.jaidani@gmail.com>
2025-09-06 15:27:40 +0200

    50

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -4,12 +4,38 @@ using namespace std;
 
 
 
+
+
+
 int main()
 {
-	char N = 65;
-	while (N <= 90)
+	int PIN = 1234, input = 0 , counter = 0;
+	
+	while (PIN != input & counter < 3)
 	{
-		cout << N <<endl;
-		N++;
+		cout << "Please enter your PIN: ";
+		cin >> input;
+		counter++;
+
+		while (PIN != input & counter < 3)
+		{
+			cout << "Wrong code";
+			cout << endl;
+			break;
+		}
+	}
+	
+	while (PIN == input & counter < 3)
+	{
+		cout << "Your Balance is 7500";
+		cout << endl;
+		break;
+	}
+	
+	while (PIN != input & counter == 3)
+	{
+		cout << "Card Locked";
+		cout << endl;
+		break;
 	}
 }
