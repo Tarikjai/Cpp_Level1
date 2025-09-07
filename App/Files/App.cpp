b86fab9df265a3bac52f9d03363b30fa970804cb
Tarik <tarik.jaidani@gmail.com>
2025-09-07 12:02:30 +0200

    52

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -3,39 +3,15 @@
 using namespace std;
 
 
+int arr[10] = { 10,20,44,55,33,22,99,88,99,100 };
 
 
 
 
 int main()
 {
-	int PIN = 1234, input = 0 , counter = 0;
-	
-	while (PIN != input & counter < 3)
-	{
-		cout << "Please enter your PIN: ";
-		cin >> input;
-		counter++;
 
-		while (PIN != input & counter < 3)
-		{
-			cout << "Wrong code";
-			cout << endl;
-			break;
-		}
-	}
-	
-	while (PIN == input & counter < 3)
-	{
-		cout << "Your Balance is 7500";
-		cout << endl;
-		break;
-	}
-	
-	while (PIN != input & counter == 3)
-	{
-		cout << "Card Locked";
-		cout << endl;
-		break;
-	}
+	cout << arr[9] << endl;
+
+
 }
