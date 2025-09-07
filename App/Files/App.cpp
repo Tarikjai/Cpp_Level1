523fb42114a3afa54fc0bbe4976f810c5847d5c0
Tarik <tarik.jaidani@gmail.com>
2025-09-08 00:04:30 +0200

    All Project

--- App/Files/App.cpp
+++ App/Files/App.cpp
@@ -1,34 +1,3 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-
-int readInput(int& input)
-{
-	cout << "Please insert the number" << endl;
-	cin >> input;
-	return input;
-}
-
-int sumFunction(int sum, int input)
-{
-	for (int i = 0; i < 5; i++)
-	{
-		readInput(input);
-		if (input > 50)
-		{
-			continue;
-			
-		}
-		sum += input;
-	}
-	
-	return sum;
-}
-
-int main()
-{
-	int sum = 0, input=0;
-	
-	cout << sumFunction(sum, input);
-}
